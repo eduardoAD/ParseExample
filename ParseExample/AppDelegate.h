d8dedc39be2e88c8178274d1b593fa5aab4d292c
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-27 09:34:54 -0600

    Initial Commit

---
+++ ParseExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParseExample
+//
+//  Created by Eduardo Alvarado Díaz on 10/27/14.
+//  Copyright (c) 2014 Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
